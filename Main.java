c28a845d109a4d3bcc138573b110bdeff755f6fe
Joelantonio0 <joelantonio2020ucan@gmail.com>
2024-05-17 12:43:14 +0100

    novo

--- Main.java
+++ Main.java
@@ -17,19 +17,19 @@ public class Main {
     public static void main(String[] args) {
         Equipa equipe2 = FabricaEquipa.criarEquipa2();
         System.out.println("Jogadores:");
-        for (Jogador j : equipe2.getJogadores()){
-            System.out.println(j.getNome()+" "+j.getPosicao()+" "+j.getQualidade());
+        for (Jogador j : equipe2.getJogadores()) {
+            System.out.println(j.getNome() + " " + j.getPosicao() + " " + j.getQualidade());
         }
         List<Jogador> naoRelacionados = equipe2.relacionarJogadores();
         System.out.println("---------------------------");
         System.out.println("Relacionados:");
-        for (Jogador r : equipe2.getRelacionados()){
-            System.out.println(r.getNome()+" "+r.getPosicao()+" "+r.getQualidade());
+        for (Jogador r : equipe2.getRelacionados()) {
+            System.out.println(r.getNome() + " " + r.getPosicao() + " " + r.getQualidade());
         }
         System.out.println("---------------------------");
         System.out.println("Jogadores não relacionados:");
-        for (Jogador j : naoRelacionados){
-            System.out.println(j.getNome()+" "+j.getPosicao()+" "+j.getQualidade());
+        for (Jogador j : naoRelacionados) {
+            System.out.println(j.getNome() + " " + j.getPosicao() + " " + j.getQualidade());
         }
 
         equipe2.ImprimirEscalacoes();
